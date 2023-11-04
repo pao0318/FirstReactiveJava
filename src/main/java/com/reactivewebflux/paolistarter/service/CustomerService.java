c706bc670a3d86880ba0f58e4f87af037784dcef
pao0318 <70106499+pao0318@users.noreply.github.com>
2023-11-04 23:14:27 +0530

    added queries to find and save

--- src/main/java/com/reactivewebflux/paolistarter/service/CustomerService.java
+++ src/main/java/com/reactivewebflux/paolistarter/service/CustomerService.java
@@ -6,14 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
 
     @Autowired
-    CustomerDao customerDao;
+    private CustomerDao dao;
 
-    public Flux<Customer> loadAllCustomersStream(){
-        return customerDao.getCustomers();
 
+    public List<Customer> loadAllCustomers() {
+        long start = System.currentTimeMillis();
+        List<Customer> customers = dao.getCustomers();
+        long end = System.currentTimeMillis();
+        System.out.println("Total execution time : " + (end - start));
+        return customers;
+    }
+
+
+
+    public Flux<Customer> loadAllCustomersStream() {
+        long start = System.currentTimeMillis();
+        Flux<Customer> customers = dao.getCustomersStream();
+        long end = System.currentTimeMillis();
+        System.out.println("Total execution time : " + (end - start));
+        return customers;
     }
 }
